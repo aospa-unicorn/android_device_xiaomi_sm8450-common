cc8866d681a5a0b229f92ba675077ad73465e7e3
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-08-31 21:29:57 +0530

    sm8450-common: Make shipping API level overrideable
    Change-Id: I9863c3bc646038f5e1f944c2cd6017eea5638949

--- common.mk
+++ common.mk
@@ -393,7 +393,7 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.sensors.enable.mag_filter=true
 
 # Shipping API
-PRODUCT_SHIPPING_API_LEVEL := 31
+PRODUCT_SHIPPING_API_LEVEL ?= 31
 
 # Thermal
 PRODUCT_PACKAGES += \
