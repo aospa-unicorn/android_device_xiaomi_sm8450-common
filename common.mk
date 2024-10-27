e05e584d9ab30140c69bee53aaad2d4b20050b38
hiper25 <2243168976@qq.com>
2024-10-27 19:09:58 +0800

    Fixup

--- common.mk
+++ common.mk
@@ -433,11 +433,13 @@ PRODUCT_COPY_FILES += \
 
 # Missing
 PRODUCT_PACKAGES += \
+    libcurl.vendor \
     libjsoncpp.vendor \
     libsqlite.vendor \
     libgui_vendor \
     libaudioroute.vendor \
-    libcamera_metadata.vendor
+    libcamera_metadata.vendor \
+    libexif.vendor
 
 # Vendor blobs
 $(call inherit-product, vendor/xiaomi/sm8450-common/sm8450-common-vendor.mk)
